6cccf1b04c48b923fbcf28b117c4a72c24b7cba7
KiaraPrecopio <kiara.precopio@eldars.com.ar>
2024-01-25 12:15:56 -0300

    Primeros pasos: entorno gráfico

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.prueba.demoappjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.prueba.demoappjavafx to javafx.fxml;
+    exports org.prueba.demoappjavafx;
+}
